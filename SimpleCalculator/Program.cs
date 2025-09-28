0f12e0125bb2546280caebf5f663d2c1b91d9b86
Vindya Gardiyawasam <vindyadgardiyawasam@outlook.com>
2025-09-28 14:07:39 +0530

    main method

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Calculator calculator = new Calculator();
+            calculator.Add(5, 3);
+            calculator.Subtract(5, 3);  
+            calculator.Multiply(5, 3);  
+            calculator.Divide(5, 0);    
+            calculator.Multiply(5, 1);
+            Console.ReadLine();
         }
     }
 }
