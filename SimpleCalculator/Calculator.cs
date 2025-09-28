fc9ff1579518e6759e719b9bed7fe463e40cf4ed
Vindya Gardiyawasam <vindyadgardiyawasam@outlook.com>
2025-09-28 13:45:42 +0530

    Add project files.

---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Calculator
+    {   
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }       
+                
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }   
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }   
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be zero.");
+            }
+            return (double)a / b;
+        }
+
+
+    }
+}
