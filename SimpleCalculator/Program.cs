47d2f76e9356d63f0fcf14ff4eeb938da33cb4b1
Vindya Gardiyawasam <vindyadgardiyawasam@outlook.com>
2025-09-28 15:06:11 +0530

    changed

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,11 +6,11 @@
         {
             Console.WriteLine("Hello, World!");
             Calculator calculator = new Calculator();
-            calculator.Add(5, 3);
-            calculator.Subtract(5, 3);  
-            calculator.Multiply(5, 3);  
-            calculator.Divide(5, 0);    
-            calculator.Multiply(5, 1);
+            Console.WriteLine("Add 5 plus 3 is:"+calculator.Add(5, 3));
+            Console.WriteLine("Substract 5 minus 3 is:"+calculator.Subtract(5, 3));
+            Console.WriteLine("Multiply 5 by 3 is:" +calculator.Multiply(5, 3));
+            Console.WriteLine("Divide 5 from 2 is:" + calculator.Divide(5, 2));
+            Console.WriteLine("Multiply 5 by 3 is:" + calculator.Multiply(5, 1));
             Console.ReadLine();
         }
     }
